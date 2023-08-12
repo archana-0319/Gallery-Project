c59076fc0ef0bec628751b449ab82971a8aaf796
Archana <archanasreenivas.k@gmail.com>
2023-08-12 18:37:10 +0530

    changes made in food file

--- gallery-project/src/components/food/index.js
+++ gallery-project/src/components/food/index.js
@@ -3,6 +3,7 @@ const foodImages = [
     require('./food 2.jpeg'),
     require('./food 3.jpeg'),
     require('./food 4.jpeg'),
+    require('./food 5.jpeg')
   ];
   
   export default foodImages;
